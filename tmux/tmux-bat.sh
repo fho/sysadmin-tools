f9e24d2830fadc18e4c8bf6faed710a89b2fbe9f
Fabian Holler <git@fholler.de>
2013-06-23 12:17:20 +0200

    tmux rename bat.sh to tmux-bat.sh

--- tmux/tmux-bat.sh
+++ tmux/tmux-bat.sh
@@ -8,23 +8,23 @@ bat_pct=$(echo $acpi_out|grep -o '[0-9]*%'|tr -d '%')
 bat=$(echo $acpi_out|egrep -o '([0-9]{2}+:)+[0-9]{2}')
 
 if echo $acpi_out | grep -qi "Discharging"; then
-    charging=0
+	charging=0
 else
-    charging=1;
+	charging=1;
 fi
 
 if [ $bat_pct -gt 67 ]; then
-    sym=$bat_full
+	sym=$bat_full
 elif [ $bat_pct -gt 33 ]; then
-    sym=$bat_67
+	sym=$bat_67
 elif [ $bat_pct -lt 6 ]; then
-    sym=$bat_empty
+	sym=$bat_empty
 elif [ $bat_pct -lt 34 ]; then
-    sym=$bat_33
+	sym=$bat_33
 fi
 
 if [ $charging -eq 1 ]; then
-    echo "$sym ---"
+	echo "$sym ---"
 else
-    echo "$sym $bat"
+	echo "$sym $bat"
 fi
